aac342fa40c88b97a14c3d8a145f0d28e1b691c7
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-02-20 13:10:11 -0300

    adicionando componentes para o logo, listas e header

---
+++ src/componentes/OpcoesHeader/index.js
@@ -0,0 +1,14 @@
+import './estilo.css';
+
+const textoOpcoes = ["CATEGORIAS", "MINHA ESTANTE", "FAVORITOS"];
+
+function OpcoesHeader() {
+    return (
+        <ul className='opcoes'>
+        { textoOpcoes.map(texto => (
+          <li className='opcao'>{texto}</li>
+        ) ) }
+      </ul>
+    );
+}
+export default OpcoesHeader;
