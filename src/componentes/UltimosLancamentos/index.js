6b0ecbe9bbd10240e917130ce84eb712558b4878
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-02-22 20:22:38 -0300

    criando card reutilizavel com props

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,21 +1,14 @@
-import { livros } from "./dadosUltimosLancamentos";
 import styled from "styled-components";
+import { livros } from "./dadosUltimosLancamentos";
+import imagemLivro from "../../imagens/livro2.png";
+import { Titulo } from "../Titulo";
+import CardRecomenda from "../CardRecomenda";
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
     padding-bottom: 20px;
     display: flex;
     flex-direction: column;
-`
-
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: ${props => props.cor || "#000"};
-    font-size: ${props => props.tamanhoFonte || "18px"};
-    text-align: ${props => props.alinhamento || "center"};
-    margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -32,12 +25,20 @@ function UltimosLancamentos() {
             <Titulo 
                 cor="#EB9B00" 
                 tamanhoFonte="36px" 
-            >ÚLTIMOS LANÇAMENTOS</Titulo>
+            >
+                ÚLTIMOS LANÇAMENTOS
+            </Titulo>
             <NovosLivrosContainer>
                 { livros.map(livro => (
                     <img src={livro.src} alt="um livro"/>
                 ))}
             </NovosLivrosContainer>
+            <CardRecomenda 
+                titulo="Talvez você se interesse por..."
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação intergrada com a plataforma Google"
+                img={imagemLivro}
+            />
         </UltimosLancamentosContainer>
 
     );
