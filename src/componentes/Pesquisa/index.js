729433235507b453eb4480eb0d8356e897858a70
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-02-22 17:53:58 -0300

    buscando e mostrando livros dinamicamente

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -25,11 +25,28 @@ const Subtitulo = styled.h3`
     margin-bottom: 40px;
 `
 
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+
+    p {
+        width: 200px;
+    }
+
+    img {
+        width: 100px;
+    }
+
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([]);
-
-    console.log(livrosPesquisados);
-    
 
     return (
         <PesquisaContainer>
@@ -43,6 +60,12 @@ function Pesquisa() {
                     setLivrosPesquisados(resultadoPesquisa);
                 }}
             />
+            { livrosPesquisados.map(livro => (
+                <Resultado>
+                    <img src={livro.src} />
+                    <p>{livro.nome}</p>
+                </Resultado>
+            ) ) }
         </PesquisaContainer>
     );
 }
